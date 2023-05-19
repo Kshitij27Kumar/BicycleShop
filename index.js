c16e1d2d4355bc2fa63b2624f98f08b495c39e5d
Kshitij Kumar <kshitij27kumar@gmail.com>
2023-05-19 18:14:31 +0530

    Dynamic block created on homepage

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('http')
 const url = require('url')
 const fs = require('fs').promises
+const bicycles = require('./data/data.json')
 const server = http.createServer(async (req, res) => {
   if (req.url === '/favicon.ico') return
   const myUrl = new URL(req.url, `http://${req.headers.host}`)
@@ -9,11 +10,29 @@ const server = http.createServer(async (req, res) => {
   const id = myUrl.searchParams.get('id')
   console.log(req.url)
   if (pathname === '/') {
-    const html = await fs.readFile('./view/bicycles.html', 'utf-8')
+    let html = await fs.readFile('./view/bicycles.html', 'utf-8')
+
+    const AllMainBicycles = await fs.readFile('./view/main/bmain.html', 'utf-8')
+
+    let allBicycles = ''
+    for (let index = 0; index < 6; index++) {
+      allBicycles += AllMainBicycles
+    }
+    html = html.replace(/<%AllMainBicycles>/g, allBicycles)
     res.writeHead(200, { 'Content-Type': 'text/html' })
     res.end(html)
   } else if (pathname === '/bicycle' && id >= 0 && id <= 5) {
-    const html = await fs.readFile('./view/overview.html', 'utf-8')
+    let html = await fs.readFile('./view/overview.html', 'utf-8')
+    const bicycle = bicycles.find((b) => b.id === id)
+    console.log(bicycle)
+    html = html.replace(/<%IMAGE>/g, bicycle.image)
+    html = html.replace(/<%NAME>/g, bicycle.name)
+
+    let price = bicycle.originalPrice
+    if (bicycle.hasDiscount) {
+      price = (price * (100 - bicycle.discount)) / 100
+    }
+    html = html.replace(/<%PRICE>/g, `$${price}`)
     res.writeHead(200, { 'Content-Type': 'text/html' })
     res.end(html)
   } else if (/\.(png)$/i.test(req.url)) {
