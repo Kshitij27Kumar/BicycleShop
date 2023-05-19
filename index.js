b960c9a5e06fab8e5bbf2d261146c0977776419b
Kshitij Kumar <kshitij27kumar@gmail.com>
2023-05-19 14:53:28 +0530

    Css, images and svg rendering

--- index.js
+++ index.js
@@ -7,14 +7,27 @@ const server = http.createServer(async (req, res) => {
   console.log(myUrl)
   const pathname = myUrl.pathname
   const id = myUrl.searchParams.get('id')
+  console.log(req.url)
   if (pathname === '/') {
     const html = await fs.readFile('./view/bicycles.html', 'utf-8')
     res.writeHead(200, { 'Content-Type': 'text/html' })
     res.end(html)
-  } else if (pathname === '/bicycle' && id >= 1 && id <= 5) {
+  } else if (pathname === '/bicycle' && id >= 0 && id <= 5) {
     const html = await fs.readFile('./view/overview.html', 'utf-8')
     res.writeHead(200, { 'Content-Type': 'text/html' })
     res.end(html)
+  } else if (/\.(png)$/i.test(req.url)) {
+    const image = await fs.readFile(`./public/images/${req.url.slice(1)}`)
+    res.writeHead(200, { 'Content-Type': 'image/png' })
+    res.end(image)
+  } else if (/\.(css)$/i.test(req.url)) {
+    const css = await fs.readFile(`./public/css/index.css`)
+    res.writeHead(200, { 'Content-Type': 'text/css' })
+    res.end(css)
+  } else if (/\.(svg)$/i.test(req.url)) {
+    const svg = await fs.readFile('./public/images/icons.svg')
+    res.writeHead(200, { 'Content-Type': 'image/svg+xml' })
+    res.end(svg)
   } else {
     res.writeHead(404, { 'Content-Type': 'text/html' })
     res.end('<h1>File Not Found!</h1>')
